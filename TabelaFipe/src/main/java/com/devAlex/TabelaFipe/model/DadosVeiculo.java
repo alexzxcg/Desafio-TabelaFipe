02dab8f5d224862cf573c0b779a36cf5777eeea9
Alex <alex-xp123@hotmail.com>
2024-09-21 16:03:25 -0300

    Feito a sobrescrita do método toString da classe DadosVeiculo

--- TabelaFipe/src/main/java/com/devAlex/TabelaFipe/model/DadosVeiculo.java
+++ TabelaFipe/src/main/java/com/devAlex/TabelaFipe/model/DadosVeiculo.java
@@ -5,5 +5,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosVeiculo(@JsonAlias("codigo") String codigo,
+
                            @JsonAlias("nome") String nome) {
+    @Override
+    public String toString(){
+       return "Cód " + codigo + " Descrição: " + nome;
+    }
 }
