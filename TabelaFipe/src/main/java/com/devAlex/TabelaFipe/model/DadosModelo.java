9d62af26c3b748afb55e14aace4854b71a14446f
Alex <alex-xp123@hotmail.com>
2024-09-23 16:03:37 -0300

    Refatorado os atributos das classes DadosVeiculo/DadosModelo

--- TabelaFipe/src/main/java/com/devAlex/TabelaFipe/model/DadosModelo.java
+++ TabelaFipe/src/main/java/com/devAlex/TabelaFipe/model/DadosModelo.java
@@ -1,10 +1,9 @@
 package com.devAlex.TabelaFipe.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosModelo(
-        @JsonAlias("modelos") List<DadosVeiculo> modelos
-) {}
+public record DadosModelo(List<DadosVeiculo> modelos) {
+
+}
--- TabelaFipe/src/main/java/com/devAlex/TabelaFipe/model/DadosVeiculo.java
+++ TabelaFipe/src/main/java/com/devAlex/TabelaFipe/model/DadosVeiculo.java
@@ -1,12 +1,10 @@
 package com.devAlex.TabelaFipe.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosVeiculo(@JsonAlias("codigo") String codigo,
+public record DadosVeiculo(String codigo, String nome) {
 
-                           @JsonAlias("nome") String nome) {
     @Override
     public String toString(){
        return "Cód " + codigo + " Descrição: " + nome;
